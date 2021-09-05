98d19f9b57c0cf6e41094bdc88668f231d27554d
lawrencejews <lawrencejews1@gmail.com>
2021-09-05 18:31:39 +0900

    Configuration done & start building

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,34 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule } from '@fem/core-data';
+import { CoreStateModule } from '@fem/core-state';
+import { MaterialModule } from '@fem/material';
 import { StoreModule } from '@ngrx/store';
-import { CoreDataModule, coreDataRoutes } from '@fem/core-data';
-import { CoreStateModule, coreStateRoutes } from '@fem/core-state';
-import { RoutingModule } from './routing.module';
-import { WidgetsComponent } from './widgets/widgets.component';
-import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MaterialModule, materialRoutes } from '@fem/material';
+import { RoutingModule } from './routing.module';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     WidgetsComponent,
     WidgetsListComponent,
-    HomeComponent,
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({}, {}),
+    BrowserModule,
     CoreDataModule,
     CoreStateModule,
-    RoutingModule,
+    HttpClientModule,
     MaterialModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
