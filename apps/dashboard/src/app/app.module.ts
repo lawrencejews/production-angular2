edd8c3d4e38b3eba497abcd551a4ee0579c6afd0
lawrencejews <lawrencejews1@gmail.com>
2021-09-05 17:36:13 +0900

    first commit

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -3,10 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { CoreDataModule, coreDataRoutes } from '@fem/core-data';
+import { CoreStateModule, coreStateRoutes } from '@fem/core-state';
+import { RoutingModule } from './routing.module';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule, materialRoutes } from '@fem/material';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    WidgetsComponent,
+    WidgetsListComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    CoreStateModule,
+    RoutingModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,13 @@
 export interface Message {
   message: string;
 }
+
+
+export interface BaseEntity {
+  id: string | null;
+}
+
+export interface Widget extends BaseEntity{
+  titile: string;
+  description: string;
+}
